b2595cae0b7eb1e5a1aca1e415ff7ba17d10527e
Leo Ayala <leo_a1016@yahoo.com>
2020-07-05 12:51:25 -0400

    added all timeslots and function to save button

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,16 @@
+saveTaskEl = document.getElementById("save-Task")
+textAreaEl = document.querySelector("#Work-todo")
+var ToDo = ['','','','','','','','',''];
+
 const m = moment();
 currentDayEl = document.querySelector("#currentDay")
 currentDayEl.textContent = m.format("dddd, MMMM Do YYYY");
 
 
+$(".saveBtn").on("click", function () {
+    var time = $(this).attr("data-time");
+    var text = $("#"+ time + "-Text").val();
+    ToDo[parseInt(time)-9] = text
+    console.log(ToDo);
+    localStorage.setItem("Tasks", JSON.stringify(ToDo))
+});
